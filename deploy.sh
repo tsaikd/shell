e57b21c6086dce734260f55fc45710c66aa03b67
tsaikd <tsaikd@gmail.com>
2013-04-29 11:08:06 +0800

    support deploy to multiple host and without 22 port

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,16 @@ PD="${BASH_SOURCE[0]%/*}"
 
 pushd "${PD}" &>/dev/null
 
-tarhost="${1}"
-if [ "${tarhost}" ] ; then
-	echo "deploy to '${tarhost}'"
-	rsync -aP -e "ssh -o Ciphers=arcfour" "${PD}" "${tarhost}:/tmp/bash/" && \
-		ssh "${tarhost}" "cd /tmp/bash ; ./setrcs.sh ; cd / ; rm -rf /tmp/bash"
-fi
+for i in $@ ; do
+	tarhost="$(cut -d: -f1 <<<"${i}")"
+	tarport="$(cut -d: -f2 <<<"${i}")"
+	true ${tarport:=22}
+	if [ "${tarhost}" ] ; then
+		echo "deploy to '${tarhost}:${tarport}'"
+		rsync -aP -e "ssh -p ${tarport} -o Ciphers=arcfour" "${PD}" "${tarhost}:/tmp/bash/" && \
+			ssh "${tarhost}" -p "${tarport}" "cd /tmp/bash ; ./setrcs.sh ; cd / ; rm -rf /tmp/bash"
+	fi
+done
 
 popd &>/dev/null
 
