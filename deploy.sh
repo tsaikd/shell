672b15351f0ae264eee73909b97de4cd464378b6
tsaikd <tsaikd@gmail.com>
2013-05-09 13:31:52 +0800

    add StrictHostKeyChecking in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ PD="${BASH_SOURCE[0]%/*}"
 pushd "${PD}" &>/dev/null
 
 for i in $@ ; do
-	tarhost="$(cut -d: -f1 <<<"${i}")"
-	tarport="$(cut -d: -f2 <<<"${i}")"
+	tarhost="$(cut -d: -f1 <<<"${i}:")"
+	tarport="$(cut -d: -f2 <<<"${i}:")"
 	true ${tarport:=22}
 	if [ "${tarhost}" ] ; then
 		echo "deploy to '${tarhost}:${tarport}'"
-		rsync -aP -e "ssh -p ${tarport} -o Ciphers=arcfour" "${PD}" "${tarhost}:/tmp/bash/" && \
-			ssh "${tarhost}" -p "${tarport}" "cd /tmp/bash ; ./setrcs.sh ; cd / ; rm -rf /tmp/bash"
+		rsync -aPq -e "ssh -p ${tarport} -o Ciphers=arcfour -o StrictHostKeyChecking=no" "${PD}/" "${tarhost}:/tmp/bash/" && \
+			ssh "${tarhost}" -p "${tarport}" "cd /tmp/bash ; /tmp/bash/setrcs.sh ; cd / ; rm -rf /tmp/bash"
 	fi
 done
 
